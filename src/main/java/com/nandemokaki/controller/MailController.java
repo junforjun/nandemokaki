ad0cc090e260a92b47f5865f08ba0a8cddfdcc48
JUN <kyuno1984@gmail.com>
2017-03-02 00:48:16 +0900

    mail server commit

--- src/main/java/com/nandemokaki/controller/MailController.java
+++ src/main/java/com/nandemokaki/controller/MailController.java
@@ -1,20 +1,13 @@
 package com.nandemokaki.controller;
 
-import javax.persistence.EntityManager;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MailController {
-	@Autowired
-	private EntityManager em;
+//	@Autowired
+//	private EntityManager em;
+//
+//	@RequestMapping(value = "/login/user/create", method = RequestMethod.GET)
+//	@ResponseBody
 
-	@RequestMapping(value = "/login/user/create", method = RequestMethod.GET)
-	@ResponseBody
-	
 }
